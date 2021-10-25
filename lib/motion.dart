da3e7e6d53cad3e281f596292487b49f811203fa
harrischen <249367286@qq.com>
2021-10-25 20:23:28 +0800

    init: zoomInDown

--- lib/motion.dart
+++ lib/motion.dart
@@ -1,6 +1,7 @@
 library motion;
 
 export 'package:motion/src/zooming-entrances/zoom-in.dart';
+export 'package:motion/src/zooming-entrances/zoom-in-down.dart';
 
 ///  Author: harrischen
 ///  Version: 0.0.1
--- lib/src/zooming-entrances/zoom-in-down.dart
+++ lib/src/zooming-entrances/zoom-in-down.dart
@@ -1,1 +1,43 @@
+import 'package:flutter/material.dart';
 
+/// How to achieve zoomIn in animate.css
+/// @keyframes zoomInDown {
+///   from {
+///     opacity: 0;
+///     transform: scale3d(0.1, 0.1, 0.1) translate3d(0, -1000px, 0);
+///     animation-timing-function: cubic-bezier(0.55, 0.055, 0.675, 0.19);
+///   }
+///
+///   60% {
+///     opacity: 1;
+///     transform: scale3d(0.475, 0.475, 0.475) translate3d(0, 60px, 0);
+///     animation-timing-function: cubic-bezier(0.175, 0.885, 0.32, 1);
+///   }
+/// }
+///
+/// .zoomInDown {
+///   animation-name: zoomInDown;
+/// }
+
+class ZoomInDown extends StatefulWidget {
+  const ZoomInDown({Key? key}) : super(key: key);
+
+  @override
+  _ZoomInDownState createState() => _ZoomInDownState();
+}
+
+class _ZoomInDownState extends State<ZoomInDown> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class GrowTransition extends StatelessWidget {
+  const GrowTransition({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
