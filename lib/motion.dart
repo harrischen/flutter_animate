b2b60a06e48594ab1305056f786b8702ffb892d1
harrischen <249367286@qq.com>
2021-10-27 19:55:05 +0800

    feat: add zoomInRight widget

--- lib/motion.dart
+++ lib/motion.dart
@@ -4,6 +4,7 @@ export 'package:motion/src/zooming-entrances/zoom-in.dart';
 export 'package:motion/src/zooming-entrances/zoom-in-down.dart';
 export 'package:motion/src/zooming-entrances/zoom-in-up.dart';
 export 'package:motion/src/zooming-entrances/zoom-in-left.dart';
+export 'package:motion/src/zooming-entrances/zoom-in-right.dart';
 
 ///  Author: harrischen
 ///  Version: 0.0.1
