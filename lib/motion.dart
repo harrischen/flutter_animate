48edec900e951c1aa1546e7aa0769a37f2da32c7
harrischen <249367286@qq.com>
2021-10-27 19:45:34 +0800

    feat: add zoomInUp widget

--- lib/motion.dart
+++ lib/motion.dart
@@ -2,6 +2,7 @@ library motion;
 
 export 'package:motion/src/zooming-entrances/zoom-in.dart';
 export 'package:motion/src/zooming-entrances/zoom-in-down.dart';
+export 'package:motion/src/zooming-entrances/zoom-in-up.dart';
 
 ///  Author: harrischen
 ///  Version: 0.0.1
