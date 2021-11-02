7d353065940ae951104a1b01f69e0363ee9b8b3c
harrischen <249367286@qq.com>
2021-11-02 18:42:22 +0800

    feat: add slideInLeft widget

--- example/main.dart
+++ example/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
           title: Text('Animate Demo'),
         ),
         body: Center(
-          child: SlideInDown(
+          child: SlideInLeft(
             completed: () {
               print('=========completed=========');
             },
