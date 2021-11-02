0b65e9cbc3e1334511a2fb7671180d02d2990a70
harrischen <249367286@qq.com>
2021-11-02 18:39:06 +0800

    feat: add slideInDown widget

--- example/main.dart
+++ example/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
           title: Text('Animate Demo'),
         ),
         body: Center(
-          child: SlideInUp(
+          child: SlideInDown(
             completed: () {
               print('=========completed=========');
             },
---
+++ lib/src/sliding_entrances/slideInDown.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+class SlideInDown extends StatefulWidget {
+  const SlideInDown({
+    Key? key,
+    this.child = const Text(
+      'SlideInDown',
+      style: TextStyle(
+        fontSize: 40,
+        fontWeight: FontWeight.bold,
+        color: Colors.lightBlue,
+      ),
+    ),
+    this.duration = const Duration(milliseconds: 1000),
+    this.delay = const Duration(milliseconds: 0),
+    this.curve = Curves.ease,
+    this.completed,
+  }) : super(key: key);
+
+  final Widget child;
+  final Duration duration;
+  final Duration delay;
+  final Curve curve;
+  final VoidCallback? completed;
+
+  @override
+  _SlideInDownState createState() => _SlideInDownState();
+}
+
+class _SlideInDownState extends State<SlideInDown>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> offset;
+  bool visible = false;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(duration: widget.duration, vsync: this)
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed &&
+            widget.completed is Function) {
+          widget.completed!();
+        }
+      });
+
+    offset = Tween<double>(begin: 1.0, end: 0.0).animate(CurvedAnimation(
+      parent: controller,
+      curve: widget.curve,
+    ));
+
+    Future.delayed(widget.delay, () {
+      setState(() {
+        visible = true;
+      });
+      controller.forward();
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _GrowTransition(
+      child: widget.child,
+      controller: controller,
+      offset: offset,
+      visible: visible,
+    );
+  }
+}
+
+class _GrowTransition extends StatelessWidget {
+  const _GrowTransition({
+    Key? key,
+    required this.controller,
+    required this.child,
+    required this.offset,
+    required this.visible,
+  }) : super(key: key);
+
+  final Widget child;
+  final AnimationController controller;
+  final Animation<double> offset;
+  final bool visible;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: controller,
+      builder: (context, child) {
+        return Visibility(
+          visible: visible,
+          child: FractionalTranslation(
+            translation: Offset(0.0, -offset.value),
+            child: child,
+          ),
+        );
+      },
+      child: child,
+    );
+  }
+}
