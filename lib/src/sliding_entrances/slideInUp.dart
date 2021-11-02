c2dca0afbe54ca23be169a492bdce648ae950fec
harrischen <249367286@qq.com>
2021-11-02 18:24:38 +0800

    feat: add slideInUp widget

---
+++ lib/src/sliding_entrances/slideInUp.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+class SlideInUp extends StatefulWidget {
+  const SlideInUp({
+    Key? key,
+    this.child = const Text(
+      'SlideInUp',
+      style: TextStyle(
+        fontSize: 40,
+        fontWeight: FontWeight.bold,
+        color: Colors.lightBlue,
+      ),
+    ),
+    this.duration = const Duration(milliseconds: 1000),
+    this.delay = const Duration(milliseconds: 0),
+    this.curve = Curves.ease,
+    this.completed,
+  }) : super(key: key);
+
+  final Widget child;
+  final Duration duration;
+  final Duration delay;
+  final Curve curve;
+  final VoidCallback? completed;
+
+  @override
+  _SlideInUpState createState() => _SlideInUpState();
+}
+
+class _SlideInUpState extends State<SlideInUp>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> offset;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(duration: widget.duration, vsync: this)
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed &&
+            widget.completed is Function) {
+          widget.completed!();
+        }
+      });
+
+    offset = Tween<double>(begin: 1.0, end: 0.0).animate(CurvedAnimation(
+      parent: controller,
+      curve: widget.curve,
+    ));
+
+    Future.delayed(widget.delay, () {
+      controller.forward();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _GrowTransition(
+      child: widget.child,
+      controller: controller,
+      offset: offset,
+    );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+}
+
+class _GrowTransition extends StatelessWidget {
+  const _GrowTransition({
+    Key? key,
+    required this.controller,
+    required this.child,
+    required this.offset,
+  }) : super(key: key);
+
+  final Widget child;
+  final AnimationController controller;
+  final Animation<double> offset;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: controller,
+      builder: (context, child) {
+        return FractionalTranslation(
+          translation: Offset(0.0, offset.value),
+          child: child,
+        );
+      },
+      child: child,
+    );
+  }
+}
