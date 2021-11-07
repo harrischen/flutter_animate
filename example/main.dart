42d77164498d9935134df9682d114083a7b46bfc
harrischen <249367286@qq.com>
2021-11-08 07:52:55 +0800

    feat: add JackInTheBox widget

--- example/main.dart
+++ example/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
           title: Text('Animate Demo'),
         ),
         body: Center(
-          child: RotateOutUpLeft(
+          child: JackInTheBox(
             completed: () {
               print('=========completed=========');
             },
